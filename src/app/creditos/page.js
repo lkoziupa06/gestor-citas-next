45226ae7e2a08d36ef0b177bce6bed8c6e26f5bc
LUCAS NICOLAS KOZIUPA <47572827@ort>
2024-07-31 14:28:31 -0300

    avances

--- src/app/creditos/page.js
+++ src/app/creditos/page.js
@@ -2,12 +2,12 @@ import { Card } from "../components/Card";
 import lucas from "../../../public/images/lucas.jpg"
 import tomas from "../../../public/images/tomas.jpg"
 import thiago from "../../../public/images/thiago.jpg"
-import styles from "./creditos.module.css";
+import "../globals.css"
 
 export default function creditos(){
     return(
         <>
-            <div className={styles.cardContainer}>
+            <div className="cardContainer">
                 <Card title="Lucas Koziupa" imageSrc={lucas} description="bebeeeee"/>
                 <Card title="Tomas Czernuszka" imageSrc={tomas} description="bebeeeee"/>
                 <Card title="Thiago Palachi" imageSrc={thiago} description="bebeeeee"/>
